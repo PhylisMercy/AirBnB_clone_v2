59465610c47a438862b63c9da573d774c046a511
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2023-05-23 11:41:35 +0300

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue May 9 10:30:15 2023
-
-@author: Phylis Mercy
+script that starts a Flask web application:
+Routes:
+* /: display “Hello HBNB!”
+* /hbnb: display “HBNB”
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Start a basic Flask web application"""
+def hello_HBNB():
+    """
+    Print a string
+    """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Adding a specific route /hbnb"""
+@app.route('/hbnb/', strict_slashes=False)
+def display_HBNB():
+    """
+    Print a string
+    """
     return 'HBNB'
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
