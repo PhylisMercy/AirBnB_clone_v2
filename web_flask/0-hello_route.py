ea4ea798373fd7c2654511b7de21ea1ad99c220d
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2023-05-23 11:41:08 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Apr 9 8:48:00 2023
-
-@author: Phylis Mercy
+script that starts a Flask web application:
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Start a basic Flask web application"""
+def hello_HBNB():
+    """
+    Print a string
+    """
     return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
