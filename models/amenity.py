bf47f5139ed837de296600095aaeae0a11f64657
Phylis Mercy Njoroge <110587824+PhylisMercy@users.noreply.github.com>
2023-05-23 11:16:52 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class that inherits from BaseModel and Base"""
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
